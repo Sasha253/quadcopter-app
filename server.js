7585dfd208da699ce54a5b9b3c2a01b8730d5c60
Sasha253 <sashmul055@gmail.com>
2021-09-23 09:56:49 +0200

    adding quadcopter-app

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = 8000
+const path = require('path')
+const router = express.Router();
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+ extended: true})); 
+app.use(cors())
+
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/src/main.html'))
+    })
+
+router.get('/connection', function(req, res){
+res.sendFile(path.join(__dirname+'/src/bleconnection.html'))
+})
+
+router.get('/dev', function(req, res){
+res.sendFile(path.join(__dirname+'/src/devmain.html'))
+})
+
+router.get('/video-feed', function(req, res){
+    res.sendFile(path.join(__dirname+'/src/devmain.html'))
+    })
+
+app.use('/', router);
+app.use("/static", express.static('./static/'));
+app.listen(port, ()=>{
+    console.log(`Server is runing on port ${port}`)
+})
---
+++ static/logic.js
@@ -0,0 +1,33 @@
+let bleConnectionButton = document.querySelector('.bleConnectionButton');
+let bleInitialiseButton = document.querySelector('.bleInitialiseButton');
+bleConnectionButton.addEventListener('click',function(){
+
+    console.log('new switch click, connect');
+    led.connect()
+        .then(() => {
+          let connected = document.querySelector("h2");
+          connected.innerHTML = "connected";
+          bleConnectionButton.stlye.display = "none"
+          bleInitialiseButton.stlye.display = "block"
+          console.log('connected')})
+        .catch(error => { console.log('connect error!');
+      });
+  });
+
+function powerOn() {
+    led._writeCharacteristic(led.characteristic1UUID, new Uint8Array([1]))
+      .then(() => console.log('wrote 1'))
+      .catch(error => {console.log('write error');
+    });
+}
+
+function powerOff() {
+    led._writeCharacteristic(led.characteristic1UUID, new Uint8Array([0]))
+      .then(() => console.log('wrote 0'))
+      .catch(error => {console.log('write error');
+    });
+}
+
+function initialise() {
+  location.href = "http://localhost:8000/"
+}
